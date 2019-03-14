6efb69aa02bc1cce01eae58455293e8de38e5404
Hubert Banaszewski <hubert.banaszewski@golem.network>
2019-03-14 15:51:00 +0100

    Revert unnecessary echo and line

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,5 @@
 set -x
 GIT_BRANCH=${1:-"gu-gateway"}
 
-echo ${GIT_BRANCH}
 docker build --build-arg GIT_BRANCH="$GIT_BRANCH" step1 -t golem-dockerized:step1 && sh step2.sh && docker build step3-gw -t golem-dockerized:step3-gw
 
