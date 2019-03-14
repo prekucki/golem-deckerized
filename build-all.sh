e3bd49f01bce26e6d50b7cb93d459c794205d6e2
destruktiw <hubert.banaszewski@golem.network>
2019-03-14 15:47:20 +0100

    Gateway version without env variable selection of build type

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,6 @@
 set -x
-GIT_BRANCH=${1:-"0.19.0"}
+GIT_BRANCH=${1:-"gu-gateway"}
+
+echo ${GIT_BRANCH}
 docker build --build-arg GIT_BRANCH="$GIT_BRANCH" step1 -t golem-dockerized:step1 && sh step2.sh && docker build step3-gw -t golem-dockerized:step3-gw
 
--- step3-gw/run-golem.sh
+++ step3-gw/run-golem.sh
@@ -1,28 +1,5 @@
 #! /bin/bash
 
-info() {
-	echo $@ >&2
-}
-
-if test -c /dev/fuse
-then
-	info Priviliged mode: starting dockerd
-	/usr/bin/dockerd &
-	sleep 5
-elif test -S /var/run/docker.sock 
-then
-	info Docker passthrough
-	docker version >/dev/null || exit 1
-else
-	info "Docker in docker required!"
-	info "run container with:"
-	info "docker run -v /var/run/docker.sock:/var/run/docker.sock ..."
-	info " or "
-	info "docker run --privileged ..."
-	info ""
-	exit 1
-fi
-
 source /work/venv/bin/activate
 cd /work/golem
 export LC_ALL=C.UTF-8
